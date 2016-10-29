35ce87d85e21352b3af32fce7c16e07cb34fe9ee
Gilles Degottex <gilles.degottex@gmail.com>
2016-10-29 23:23:22 +0100

    init test

---
+++ test/test_version.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE TestRangedArray
+#include <boost/test/unit_test.hpp>
+
+#include <fftscarf.h>
+
+BOOST_AUTO_TEST_CASE( test_rangef )
+{
+    std::cout << "Testing FFScarf compilation..." << std::endl;
+
+    // Print some info related to versions and compilation
+    fftscarf::write_compile_info(cout);
+}
